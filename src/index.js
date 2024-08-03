22c8bf01fafcb85d29453c21be87d2fb70658b49
Kotofeishe <ksenia.migunova91@yandex.ru>
2024-08-03 17:32:28 +0300

    well done reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function reverse (n) {
-  
+function reverseString (string) {
+    let i = 0;
+    let resultReverseString = '';
+    while (i < string.length) {
+        resultReverseString = resultReverseString + string[string.length-1-i];
+        i = i + 1;
+    }
+    return (resultReverseString);
+
+    
 }
+module.exports = function reverse (n) {
+        n = n.toString();
+        if(n < 0) {
+        n = n.slice(1);
+      }
+      let result = reverseString(n);
+      
+      while (+result[0] === 0) {
+        result = result.slice(1);
+       
+      }
+      return(result);
+}
+
+
+
+
